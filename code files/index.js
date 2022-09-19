a9c2364c5d26fbd8e88fb067fbe508244e5b52d3
Phil <produktivemail@googlemail.com>
2022-09-19 19:15:46 +0200

    upload full signup form files

---
+++ code files/index.js
@@ -0,0 +1,10 @@
+let password1 = document.querySelector(".password1");
+let password2 = document.querySelector(".password2");
+let submit = document.querySelector(".create-acc");
+
+
+if(password1.value !== "" && password2.value !== ""){
+   if(password1.value == password2.value){
+    alert("pog");
+   }
+}
